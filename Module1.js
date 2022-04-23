3212bfa93cf62af8d5ac94f3ffd7513a196b3367
Runner <mintheinkyaw878@gmail.com>
2022-04-23 16:44:24 +0630

    Finish day

---
+++ Module1.js
@@ -0,0 +1,5 @@
+const Min_Thein_Kyaw = "Min Thein Kyaw"
+const Runner = "Runner"
+const Zed = "Zed"
+
+module.exports = { Min_Thein_Kyaw, Runner, Zed }
---
+++ Module3.js
@@ -0,0 +1,6 @@
+const sayHi = require('./Module2')
+const { Min_Thein_Kyaw, Runner, Zed } = require('./Module1')
+
+sayHi(Min_Thein_Kyaw)
+sayHi(Runner)
+sayHi(Zed)
